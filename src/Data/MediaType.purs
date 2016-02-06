7adee78e283fc787db5df000dbecc6e91125b565
Gary Burgess <gary.burgess@gmail.com>
2016-02-06 03:51:30 +0000

    Add Ord instance

--- src/Data/MediaType.purs
+++ src/Data/MediaType.purs
@@ -7,6 +7,9 @@ newtype MediaType = MediaType String
 instance eqMediaType :: Eq MediaType where
   eq (MediaType x) (MediaType y) = x == y
 
+instance ordMediaType :: Ord MediaType where
+  compare (MediaType x) (MediaType y) = compare x y
+
 instance showMediaType :: Show MediaType where
   show (MediaType h) = "(MediaType " <> show h <> ")"
 
